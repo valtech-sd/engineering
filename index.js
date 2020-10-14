d3e190991ab9b952cbdb623dfd99fe13f34c3636
Matthew Morey <me@matthewmorey.com>
2020-10-14 09:37:03 -0700

    Added API and example use case.

---
+++ index.js
@@ -0,0 +1,24 @@
+async function getPrototypes() {
+	let url = 'api/prototypes.json';
+
+	try {
+		let response = await fetch(url);
+		return await response.json();
+	} catch(error) {
+		console.log(error);
+	}
+}
+
+async function renderPrototypes() {
+	let prototypes = await getPrototypes();
+	let html = '';
+	prototypes.prototypes.forEach(prototype => {
+		let htmlSegment = `<li>${prototype.name}</li>`;
+		html += htmlSegment;
+	});
+
+	let prototypesContainer = document.querySelector('.prototypesContainer');
+	prototypesContainer.innerHTML = html;
+}
+
+renderPrototypes();
