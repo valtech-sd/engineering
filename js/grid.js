ee0c8985e33a29a7a3f5ac73662dc70c981d118f
Tim Johnson <tim.johnson@mjdinteractive.com>
2020-10-20 15:38:14 -0700

    Fixed a grid video playback bug

--- js/grid.js
+++ js/grid.js
@@ -12,28 +12,34 @@ function throttle(callback, limit) {
 }
 
 imagesLoaded('.grid', function () {
+  checkVisibilityOfAllVideos();
+
   new Colcade('.grid', {
     columns: '.grid-col',
     items: '.grid-item',
   });
+
+  checkVisibilityOfAllVideos();
 });
+
+function playVideoIfVisible(video) {
+  var rect = video.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+  var isPartiallyVisible = elemTop < window.innerHeight && elemBottom >= 0;
+  // var isCompletelyVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+  if (isPartiallyVisible) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 function checkVisibilityOfAllVideos() {
   document.querySelectorAll('.grid video').forEach(function (video) {
-    var rect = video.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
-    var isPartiallyVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    // var isCompletelyVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
-
-    if (isPartiallyVisible) {
-      video.play();
-    } else {
-      video.pause();
-    }
+    playVideoIfVisible(video);
   });
 }
 
 window.addEventListener('scroll', throttle(checkVisibilityOfAllVideos, 250));
-
-checkVisibilityOfAllVideos();
