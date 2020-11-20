18e9ac238e513a410315765aeb014cbac3ac8e9b
jlgonzalez099 <jose.gonzalez@valtech.com>
2020-11-20 14:48:57 +0530

    Updated Js to inject GA and Marketo scripts
    updated cookie js and removed analytics.html from default.html

--- js/cookie.js
+++ js/cookie.js
@@ -1,9 +1,24 @@
 function setCookie(e) {
-	if(e === 'true'){
-      var d = new Date();     
-      d.setFullYear(d.getFullYear() + 2);
-      var expires = "expires="+d.toUTCString();
-      document.cookie = "V_Innovation_accept" + "=" + e + "; " + expires;  
+	if(e === 'false'){
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-97923818-10";
+    script.async=true;
+    document.head.appendChild(script);
+
+    var gtm = "window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-97923818-10');";
+    var script = document.createElement('script');
+    script.innerHTML = gtm;
+    document.head.appendChild(script);
+
+    var marketo ="(function(){var didInit=false;function initMunchkin(){if(didInit === false){didInit=true;Munchkin.init('353-SAE-487');}}var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src='//munchkin.marketo.net/munchkin.js';s.onreadystatechange=function(){if(this.readyState=='complete'||this.readyState=='loaded'){initMunchkin();}};s.onload=initMunchkin;document.getElementsByTagName('head')[0].appendChild(s);})();";
+    var script = document.createElement('script');
+    script.innerHTML = marketo;
+    document.head.appendChild(script);
+
+    var d = new Date();     
+    d.setFullYear(d.getFullYear() + 2);
+    var expires = "expires="+d.toUTCString();
+    document.cookie = "V_Innovation_accept" + "=" + e + "; " + expires;  
 	}
 	 document.getElementById("cookiewrapper").style.display = "none";
 }
