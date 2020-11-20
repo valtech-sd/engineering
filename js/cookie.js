259d1bb8d3e5da616c47f8c883419fe353cb52af
jlgonzalez099 <jose.gonzalez@valtech.com>
2020-11-20 23:45:44 +0530

    update cookie js
    If user has accepted cookies , then in next visit the marketo/GA scripts should be injected. Logic is based on the existence of v_innovation_accept cookie

--- js/cookie.js
+++ js/cookie.js
@@ -1,20 +1,6 @@
 function setCookie(e) {
 	if(e === 'true'){
-    var script = document.createElement('script');
-    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-97923818-10";
-    script.async=true;
-    document.head.appendChild(script);
-
-    var gtm = "window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-97923818-10');";
-    var script = document.createElement('script');
-    script.innerHTML = gtm;
-    document.head.appendChild(script);
-
-    var marketo ="(function(){var didInit=false;function initMunchkin(){if(didInit === false){didInit=true;Munchkin.init('353-SAE-487');}}var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src='//munchkin.marketo.net/munchkin.js';s.onreadystatechange=function(){if(this.readyState=='complete'||this.readyState=='loaded'){initMunchkin();}};s.onload=initMunchkin;document.getElementsByTagName('head')[0].appendChild(s);})();";
-    var script = document.createElement('script');
-    script.innerHTML = marketo;
-    document.head.appendChild(script);
-
+    createMarketoGAScript();
     var d = new Date();     
     d.setFullYear(d.getFullYear() + 2);
     var expires = "expires="+d.toUTCString();
@@ -35,7 +21,25 @@ function getCookie(cname) {
 
 function checkCookies() {  
   var c = getCookie('V_Innovation_accept');
-  if(c){	 
-	  document.getElementById("cookiewrapper").style.display = "none";
+  if(c){
+    createMarketoGAScript();
+    document.getElementById("cookiewrapper").style.display = "none";
   }
+}
+
+function createMarketoGAScript(){
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-97923818-10";
+    script.async=true;
+    document.head.appendChild(script);
+
+    var gtm = "window.dataLayer = window.dataLayer || [];function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'UA-97923818-10');";
+    var script = document.createElement('script');
+    script.innerHTML = gtm;
+    document.head.appendChild(script);
+
+    var marketo ="(function(){var didInit=false;function initMunchkin(){if(didInit === false){didInit=true;Munchkin.init('353-SAE-487');}}var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src='//munchkin.marketo.net/munchkin.js';s.onreadystatechange=function(){if(this.readyState=='complete'||this.readyState=='loaded'){initMunchkin();}};s.onload=initMunchkin;document.getElementsByTagName('head')[0].appendChild(s);})();";
+    var script = document.createElement('script');
+    script.innerHTML = marketo;
+    document.head.appendChild(script);
 }
